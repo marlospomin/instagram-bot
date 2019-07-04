b8836b73a63d651899358a45681a8ba563792e57
Marlos Pomin <marlospomin@gmail.com>
2019-07-03 21:42:20 -0300

    Add install script

---
+++ setup/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Update apt repository
+apt update
+# Install required packages
+apt install unzip python3-pip python3-dev build-essential libssl-dev libffi-dev xvfb -y
+# Upgrade pip and setuptools
+pip3 install --upgrade pip setuptools
+# Move to user's home folder
+cd ~
+# Clone chrome debian package
+wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+# Install chrome
+dpkg -i google-chrome-stable_current_amd64.deb
+# Install chrome dependencies
+apt install -y -f
+# Delete installation file
+rm google-chrome-stable_current_amd64.deb
+# Install instapy
+pip3 install instapy
